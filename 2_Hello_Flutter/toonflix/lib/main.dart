7b3f78f93d1edabfd2b49cc184465153277d0cb1
rednada1486 <rednada1486@gmail.com>
2024-10-28 03:14:02 +0900

    3-19 Request 버튼 만들기

--- 2_Hello_Flutter/toonflix/lib/main.dart
+++ 2_Hello_Flutter/toonflix/lib/main.dart
@@ -54,7 +54,9 @@ class App extends StatelessWidget {
           // Center는 child를 가운데로 오게하는 Widget이다.
           backgroundColor: const Color(0xFF181818),
           body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -115,6 +117,7 @@ class App extends StatelessWidget {
                   height: 30,
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     // Container는 일종의 div
                     // Container를
@@ -122,7 +125,7 @@ class App extends StatelessWidget {
                     // 꾸며줄 수 있다.
                     Container(
                       decoration: BoxDecoration(
-                        color: Colors.amber,
+                        color: const Color(0xFFF1B33B),
                         borderRadius: BorderRadius.circular(45),
                       ),
                       child: const Padding(
@@ -133,7 +136,26 @@ class App extends StatelessWidget {
                         child: Text(
                           'Transfer',
                           style: TextStyle(
-                            fontSize: 22,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF1F2123),
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Request',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
                           ),
                         ),
                       ),
