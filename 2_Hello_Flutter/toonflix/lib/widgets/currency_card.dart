3bc5efe98381966f621e255c96dade2a43fab1d7
rednada1486 <rednada1486@gmail.com>
2024-10-29 02:19:29 +0900

    3-32 'Bitcoin' Card 색반전 시키기 (CurrencyCard 클래스에 isInvertd 프로퍼티 도입)

--- 2_Hello_Flutter/toonflix/lib/widgets/currency_card.dart
+++ 2_Hello_Flutter/toonflix/lib/widgets/currency_card.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class CurrencyCard extends StatelessWidget {
   final String name, code, amount;
   final IconData icon;
+  final bool isInverted;
+
+  // 프로퍼티 앞에 _(언더바)를 하면
+  // 해당 프로퍼티는 private로 지정되며
+  // 외부에 공개되지 않는다.
+  final _blackColor = const Color(0xFF1F2123);
 
   const CurrencyCard({
     super.key,
@@ -10,6 +16,7 @@ class CurrencyCard extends StatelessWidget {
     required this.code,
     required this.amount,
     required this.icon,
+    required this.isInverted,
   });
 
   @override
@@ -22,7 +29,7 @@ class CurrencyCard extends StatelessWidget {
       // Clip.hardEdge -> overflow 된 부분 사라지게함
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
-        color: const Color(0xFF1F2123),
+        color: isInverted ? Colors.white : _blackColor,
         borderRadius: BorderRadius.circular(25),
       ),
       child: Padding(
@@ -35,8 +42,8 @@ class CurrencyCard extends StatelessWidget {
               children: [
                 Text(
                   name,
-                  style: const TextStyle(
-                    color: Colors.white,
+                  style: TextStyle(
+                    color: isInverted ? _blackColor : Colors.white,
                     fontSize: 32,
                     fontWeight: FontWeight.w600,
                   ),
@@ -48,8 +55,8 @@ class CurrencyCard extends StatelessWidget {
                   children: [
                     Text(
                       amount,
-                      style: const TextStyle(
-                        color: Colors.white,
+                      style: TextStyle(
+                        color: isInverted ? _blackColor : Colors.white,
                         fontSize: 20,
                       ),
                     ),
@@ -59,7 +66,9 @@ class CurrencyCard extends StatelessWidget {
                     Text(
                       code,
                       style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
+                        color: isInverted
+                            ? _blackColor.withOpacity(0.8)
+                            : Colors.white.withOpacity(0.8),
                         fontSize: 20,
                       ),
                     ),
@@ -78,7 +87,7 @@ class CurrencyCard extends StatelessWidget {
                 offset: const Offset(-5, 12),
                 child: Icon(
                   icon,
-                  color: Colors.white,
+                  color: isInverted ? _blackColor : Colors.white,
                   size: 88,
                 ),
               ),
