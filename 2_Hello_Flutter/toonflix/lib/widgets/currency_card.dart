5c604aa76fd89b26c8b8c51a6468472f05ff0d89
rednada1486 <rednada1486@gmail.com>
2024-10-29 02:36:02 +0900

    3-36 CurrencyCard 클래스에 offset 프로퍼티 도입하여 코드 리팩토링

--- 2_Hello_Flutter/toonflix/lib/widgets/currency_card.dart
+++ 2_Hello_Flutter/toonflix/lib/widgets/currency_card.dart
@@ -4,6 +4,7 @@ class CurrencyCard extends StatelessWidget {
   final String name, code, amount;
   final IconData icon;
   final bool isInverted;
+  final int offset;
 
   // 프로퍼티 앞에 _(언더바)를 하면
   // 해당 프로퍼티는 private로 지정되며
@@ -17,82 +18,86 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.isInverted,
+    required this.offset,
   });
 
   @override
   Widget build(Object context) {
-    return Container(
-      // clipBehavior는
-      // Container의 어떤 아이템이
-      // overflow가 되었을 때
-      // 어떻게 처리할 것인지 설정하는 속성
-      // Clip.hardEdge -> overflow 된 부분 사라지게함
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
+    return Transform.translate(
+      offset: Offset(0, (1 - offset) * 20),
+      child: Container(
+        // clipBehavior는
+        // Container의 어떤 아이템이
+        // overflow가 되었을 때
+        // 어떻게 처리할 것인지 설정하는 속성
+        // Clip.hardEdge -> overflow 된 부분 사라지게함
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: isInverted ? _blackColor : Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? _blackColor : Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? _blackColor.withOpacity(0.8)
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // Transform.scale
+              // 부모에 영향을 끼치지 않고
+              // 자식 요소의 크기를 바꿀 수 있다.
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  // Transform.translate로 child를
+                  // 오른쪽으로 8, 왼쪽으로 15만큼 이동
+                  offset: const Offset(-5, 12),
+                  child: Icon(
+                    icon,
                     color: isInverted ? _blackColor : Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
+                    size: 88,
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? _blackColor.withOpacity(0.8)
-                            : Colors.white.withOpacity(0.8),
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // Transform.scale
-            // 부모에 영향을 끼치지 않고
-            // 자식 요소의 크기를 바꿀 수 있다.
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                // Transform.translate로 child를
-                // 오른쪽으로 8, 왼쪽으로 15만큼 이동
-                offset: const Offset(-5, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 88,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
