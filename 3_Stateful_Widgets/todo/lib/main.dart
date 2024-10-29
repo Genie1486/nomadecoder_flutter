a4e5eba3e4b66c8344856be34dfa84b6dbcfbc67
rednada1486 <rednada1486@gmail.com>
2024-10-29 23:11:09 +0900

    5-4 날짜 위젯 부분 완성

--- 3_Stateful_Widgets/todo/lib/main.dart
+++ 3_Stateful_Widgets/todo/lib/main.dart
@@ -18,35 +18,71 @@ class _AppState extends State<App> {
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 5,
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset(
-                    'assets/images/profile_picture.png', // 커스텀 이미지 경로
-                    width: 80.0, // 너비
-                    height: 80.0, // 높이
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                    child: Icon(
-                      Icons.add,
-                      size: 30,
-                      color: Colors.white,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/images/profile_picture.png', // 커스텀 이미지 경로
+                      width: 80.0, // 너비
+                      height: 80.0, // 높이
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: Icon(
+                        Icons.add,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'MONDAY 16',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Text(
+                                  'TODAY 17 18 19 20 21 22 23 24 25 26 27 28 29 30',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w400,
+                                  )),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
