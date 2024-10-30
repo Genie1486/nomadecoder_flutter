3db18f04ba3a7cdc5032fc842f8fef2816c38021
rednada1486 <rednada1486@gmail.com>
2024-10-31 05:14:42 +0900

    7-2 Widget key 개념 정리

--- 6_WEBTOON_APP/webtoon/lib/main.dart
+++ 6_WEBTOON_APP/webtoon/lib/main.dart
@@ -5,6 +5,11 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  // Widget은 key라는 것을 가지고 있고
+  // 이 key를 ID처럼 사용한다.
+
+  // 이 Widget의 key를
+  // StatelessWidget이라는 super classd에게 보내는 부분
   const App({super.key});
 
   @override
