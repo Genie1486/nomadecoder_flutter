15e74d935baee26d2921159b67241adc52384b58
rednada1486 <rednada1486@gmail.com>
2024-10-31 05:25:41 +0900

    7-4 appBar 배경 및 글자 서식 지정

--- 6_WEBTOON_APP/webtoon/lib/screens/home_screen.dart
+++ 6_WEBTOON_APP/webtoon/lib/screens/home_screen.dart
@@ -8,7 +8,15 @@ class HomeScreen extends StatelessWidget {
     // Scaffold는 screen을 위한 기본적인 레이아웃과 설정을 제공
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Today's 툰s"),
+        elevation: 2, // 밑에 그림자
+        backgroundColor: Colors.white, // AppBar의 배경색
+        foregroundColor: Colors.green, // AppBar의 글자색 설정
+        title: const Text(
+          "Today's 툰s",
+          style: TextStyle(
+            fontSize: 26,
+          ),
+        ),
       ),
     );
   }
