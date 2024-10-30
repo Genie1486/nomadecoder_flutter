fd2a397c525005740a33802baf9954c1b2cc905e
rednada1486 <rednada1486@gmail.com>
2024-10-31 07:03:28 +0900

    7-13 WebtoonModel 주석 작성

--- 6_WEBTOON_APP/webtoon/lib/models/webtoon_model.dart
+++ 6_WEBTOON_APP/webtoon/lib/models/webtoon_model.dart
@@ -2,6 +2,7 @@ class WebtoonModel {
   final String title, thumb, id;
 
   // named constructor (이름이 있는 클래스 생성자)
+  // json으로 부터 WebtoonModel 인스턴스를 생성할 때 사용
   WebtoonModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         thumb = json['thumb'],
