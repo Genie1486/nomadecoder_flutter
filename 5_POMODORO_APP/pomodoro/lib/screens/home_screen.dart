57355bdd181e3710276859e3325c59b0dc784133
rednada1486 <rednada1486@gmail.com>
2024-10-31 00:17:43 +0900

    6-14 'start' 버튼 누르면 타이머의 시간이 줄어드는 기능 구현

--- 5_POMODORO_APP/pomodoro/lib/screens/home_screen.dart
+++ 5_POMODORO_APP/pomodoro/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500; // 25분을 초로 환산
+
+  // Timer를 통해 정해진 간격에 한번씩 함수를 실행할 수 있다.
+  // late modifier를 써서 나중에 초기화한다고 약속함
+  late Timer timer;
+
+  // onTick함수는 시간이 흐르도록 보이게 하기 위해
+  // 1초 마다 totalSeconds에서 1을 빼고
+  // UI를 업데이트 하는 역할을 한다.
+  void onTick(Timer timer) {
+    // setState는 상태가 변경되었음을 Flutter에게 알리는 메서드
+    // 이 메서드를 호출하면 Flutter는 해당 위젯과 그 자식 위젯들을 다시 그려야 한다고 판단하고
+    // UI를 업데이트 합니다.
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    // Timer.periodic(duration, callback)은
+    // duration 마다 callback 함수를 실행한다.
+    // 여기서는 1초마다 onTick 함수를 실행하도록 지정
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 // 아래쪽 가운데에 child Widget을 위치
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '25:00',
+                  '$totalSeconds',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -45,7 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: () {},
+                onPressed: onStartPressed,
                 icon: const Icon(Icons.play_circle_fill_outlined),
               ),
             ),
@@ -58,6 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).colorScheme.surface,
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
