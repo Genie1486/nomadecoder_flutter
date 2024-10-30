154f5e01f3e512101e364bbac314d3ca88b927e0
rednada1486 <rednada1486@gmail.com>
2024-10-30 21:11:05 +0900

    6-2 Flexible 개념 설명 및 기본코드 작성

--- 5_POMODORO_APP/pomodoro/lib/screens/home_screen.dart
+++ 5_POMODORO_APP/pomodoro/lib/screens/home_screen.dart
@@ -10,6 +10,36 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          // Flexible :
+          // - 높이가 200픽셀, 너비가 100픽셀 이런 식으로
+          // - 하드 코딩하지 않고
+          // - UI를 비율에 기반해서 더 유연하게 만들 수 있게 해줌
+          Flexible(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Flexible(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Flexible(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
