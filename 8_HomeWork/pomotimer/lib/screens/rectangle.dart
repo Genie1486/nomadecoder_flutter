f5b93d34a18576d2c55c3b1aff7deabcd38c7fbf
rednada1486 <rednada1486@gmail.com>
2024-10-31 20:44:47 +0900

    8-5 타이머 UI 만 완성

---
+++ 8_HomeWork/pomotimer/lib/screens/rectangle.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Rectangle extends StatelessWidget {
+  final double width, height;
+
+  const Rectangle({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        SizedBox(
+          width: width,
+          height: height,
+        ),
+        Container(
+          width: width * 0.8,
+          height: height * 0.96,
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(5), // 여기서 사용
+          ),
+        ),
+        Container(
+          width: width * 0.9,
+          height: height * 0.93,
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+        Container(
+          width: width,
+          height: height * 0.9,
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Center(
+            child: Text(
+              '25',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.surface,
+                  fontSize: 70,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
