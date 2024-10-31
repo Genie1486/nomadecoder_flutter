057888d9daad775fa0b01213adc4027096fddcb8
rednada1486 <rednada1486@gmail.com>
2024-10-31 18:14:15 +0900

    8-3 수업 중에 작성했던 타이머 관련 코드 복붙

--- 8_HomeWork/pomotimer/lib/screens/home_screens.dart
+++ 8_HomeWork/pomotimer/lib/screens/home_screens.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,70 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes; // 25분을 초로 환산
+  bool isRunning = false;
+  int totalPomodoros = 0;
+
+  // Timer를 통해 정해진 간격에 한번씩 함수를 실행할 수 있다.
+  // late modifier를 써서 나중에 초기화한다고 약속함
+  late Timer timer;
+
+  // onTick함수는 시간이 흐르도록 보이게 하기 위해
+  // 1초 마다 totalSeconds에서 1을 빼고
+  // UI를 업데이트 하는 역할을 한다.
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      // setState는 상태가 변경되었음을 Flutter에게 알리는 메서드
+      // 이 메서드를 호출하면 Flutter는 해당 위젯과 그 자식 위젯들을 다시 그려야 한다고 판단하고
+      // UI를 업데이트 합니다.
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    // Timer.periodic(duration, callback)은
+    // duration 마다 callback 함수를 실행한다.
+    // 여기서는 1초마다 onTick 함수를 실행하도록 지정
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onResetPressed() {
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      isRunning = false;
+    });
+    timer.cancel();
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +85,56 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Column(
           children: [
             Flexible(
-              flex: 3,
+              // Flexible Widget에서
+              // flex 값을 통해
+              // 하나의 박스가 얼마나 공간을 차지하 지
+              // 비율을 정할 수 있다.
+              flex: 2,
+              child: Container(
+                  // alignment: Alignment.bottomCenter
+                  // Container가 차지하는 영역의
+                  // 아래쪽 가운데에 child Widget을 위치
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    format(totalSeconds),
+                    style: TextStyle(
+                      color: Theme.of(context).cardColor,
+                      fontSize: 89,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )),
+            ),
+            Flexible(
+              // Flexible Widget에서
+              // flex 값을 통해
+              // 하나의 박스가 얼마나 공간을 차지하 지
+              // 비율을 정할 수 있다.
+              flex: 1,
               child: Container(),
+            ),
+            Flexible(
+              flex: 1,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      iconSize: 80,
+                      color: Theme.of(context).cardColor,
+                      onPressed: isRunning ? onPausePressed : onStartPressed,
+                      icon: Icon(isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_fill_outlined),
+                    ),
+                    IconButton(
+                      iconSize: 80,
+                      color: Theme.of(context).cardColor,
+                      onPressed: onResetPressed,
+                      icon: const Icon(Icons.stop),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Flexible(
               flex: 1,
