897f2ec4596aaa85dcda54f6bf665030c4299c46
rednada1486 <rednada1486@gmail.com>
2024-11-02 01:49:44 +0900

    7-33 Hero Widget 사용해보기

--- 7_WEBTOON_APP/webtoon/lib/screens/detail_screen.dart
+++ 7_WEBTOON_APP/webtoon/lib/screens/detail_screen.dart
@@ -34,22 +34,25 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
-                      offset: const Offset(10, 10), // 그림자가 어디에 위치할지
-                      color: Colors.black.withOpacity(0.5),
-                    )
-                  ],
-                ),
-                child: Image.network(
-                  thumb,
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
+                        offset: const Offset(10, 10), // 그림자가 어디에 위치할지
+                        color: Colors.black.withOpacity(0.5),
+                      )
+                    ],
+                  ),
+                  child: Image.network(
+                    thumb,
+                  ),
                 ),
               ),
             ],
--- 7_WEBTOON_APP/webtoon/lib/widgets/webtoon_widget.dart
+++ 7_WEBTOON_APP/webtoon/lib/widgets/webtoon_widget.dart
@@ -38,24 +38,29 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          // Image.network(src)는
-          // 네트워크 주소에 있는 이미지를 가져온다.
-          Container(
-            width: 250,
-            // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
-                  offset: const Offset(10, 10), // 그림자가 어디에 위치할지
-                  color: Colors.black.withOpacity(0.5),
-                )
-              ],
-            ),
-            child: Image.network(
-              thumb,
+          // Hero Widget은 화면을 전환할 때 멋진 애니메이션을 제공해준다.
+          // 파워포인트의 모핑효과 비슷
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
+                    offset: const Offset(10, 10), // 그림자가 어디에 위치할지
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              // Image.network(src)는
+              // 네트워크 주소에 있는 이미지를 가져온다.
+              child: Image.network(
+                thumb,
+              ),
             ),
           ),
           const SizedBox(
