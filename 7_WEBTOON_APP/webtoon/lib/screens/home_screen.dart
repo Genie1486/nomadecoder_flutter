7edcb33761db34e5fe9a527480fd24abfad1642a
rednada1486 <rednada1486@gmail.com>
2024-11-01 21:38:58 +0900

    7-16 7-15 에서 작성한 코드를 FutureBuilder Widget을 사용해서 리팩토링 진행

--- 7_WEBTOON_APP/webtoon/lib/screens/home_screen.dart
+++ 7_WEBTOON_APP/webtoon/lib/screens/home_screen.dart
@@ -2,33 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 import 'package:webtoon/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-
-    // UI를 새로 고침하기 위해 setState() 함수를 호출
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  // StatefulWidget의 State를 쓰지 않고
+  // 1. Future 안의 데이터를 가져오고 싶다.
+  // 2. Future의 로딩상태를 build 메서드에게 알리고 싶다.
+  // 이 것을 해주는 Widget이 FutureBuilder Widget이다.
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
     // Scaffold는 screen을 위한 기본적인 레이아웃과 설정을 제공
     return Scaffold(
       backgroundColor: Colors.white,
@@ -43,6 +28,19 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.w400,
           ),
         ),
+      ),
+      // FutureBuilder(future, builder)에서
+      // FutureBuilder가 await future로 future의 값을 기다려 준다.
+      // builder는 UI를 그려주는 함수
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          // snapshot을 이용하면 Future의 상태를 알 수 있다.
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text('Loading...');
+        },
       ),
     );
   }
