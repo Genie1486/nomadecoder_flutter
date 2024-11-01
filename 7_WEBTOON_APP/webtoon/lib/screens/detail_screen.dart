bf4de10fbc10c518de3fb9823e4ce7aacfe4f6da
rednada1486 <rednada1486@gmail.com>
2024-11-02 01:34:50 +0900

    7-31 상세페이지 초안 작성

--- 7_WEBTOON_APP/webtoon/lib/screens/detail_screen.dart
+++ 7_WEBTOON_APP/webtoon/lib/screens/detail_screen.dart
@@ -26,6 +26,36 @@ class DetailScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 250,
+                // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
+                      offset: const Offset(10, 10), // 그림자가 어디에 위치할지
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
