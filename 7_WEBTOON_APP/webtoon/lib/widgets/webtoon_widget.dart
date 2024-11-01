c0968f3e042385727ce2bd57eb21dd6957d336ff
rednada1486 <rednada1486@gmail.com>
2024-11-02 01:11:51 +0900

    7-29 GestureDector로 유저 동작 감지하기

--- 7_WEBTOON_APP/webtoon/lib/widgets/webtoon_widget.dart
+++ 7_WEBTOON_APP/webtoon/lib/widgets/webtoon_widget.dart
@@ -12,38 +12,45 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Image.network(src)는
-        // 네트워크 주소에 있는 이미지를 가져온다.
-        Container(
-          width: 250,
-          // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
-                offset: const Offset(10, 10), // 그림자가 어디에 위치할지
-                color: Colors.black.withOpacity(0.5),
-              )
-            ],
+    // GestureDetector는 대부분의 동작을 감지하도록 해준다.
+    return GestureDetector(
+      // onTap은 유저가 클릭했을 때 실행될 함수를 지정
+      onTap: () {
+        print('take me homeee');
+      },
+      child: Column(
+        children: [
+          // Image.network(src)는
+          // 네트워크 주소에 있는 이미지를 가져온다.
+          Container(
+            width: 250,
+            // clipBehavior는 자식의 부모 영역 침범을 제어하는 방법
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15, // 그림자가 얼마나 멀리까지 드리울지
+                  offset: const Offset(10, 10), // 그림자가 어디에 위치할지
+                  color: Colors.black.withOpacity(0.5),
+                )
+              ],
+            ),
+            child: Image.network(
+              thumb,
+            ),
           ),
-          child: Image.network(
-            thumb,
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
