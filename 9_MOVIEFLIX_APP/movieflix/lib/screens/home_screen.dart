b8f05e2f32efc416d072b510a467b8e60ffc35fc
rednada1486 <rednada1486@gmail.com>
2024-11-02 23:51:45 +0900

    9-4 test : getPopularMovies() 가 잘 동작하는지 테스트 진행

--- 9_MOVIEFLIX_APP/movieflix/lib/screens/home_screen.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/screens/home_screen.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:movieflix/models/movie_model.dart';
+import 'package:movieflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<MovieModel>> movies = ApiService.getPopularMovies();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue, // Colors.white,
-        title: const Text("MovieFlix"),
-      ),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue, // Colors.white,
+          title: const Text("MovieFlix"),
+        ),
+        body: FutureBuilder(
+          future: movies,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  for (var movie in snapshot.data!) Text(movie.title),
+                ],
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
