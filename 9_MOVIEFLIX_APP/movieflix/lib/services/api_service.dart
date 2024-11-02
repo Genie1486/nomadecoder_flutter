5497fa2b52d30954d27c392a4ea4f0a1bfbf0520
rednada1486 <rednada1486@gmail.com>
2024-11-02 23:50:51 +0900

    9-3 feat : 영화서버로 부터 Popular Movies를 받아오기 위해 movie_model과 getPopularMovies() 메서드 작성

---
+++ 9_MOVIEFLIX_APP/movieflix/lib/services/api_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:movieflix/models/movie_model.dart';
+
+class ApiService {
+  static const String baseUrl = "https://movies-api.nomadcoders.workers.dev";
+  static const String popular = "popular";
+
+  // getPopularMovies()는
+  // 서버로 부터 Popular Movies에 대한 정보를 가져와서
+  // 리스트로 반환하는 함수
+  static Future<List<MovieModel>> getPopularMovies() async {
+    List<MovieModel> movieInstances = [];
+    final url = Uri.parse('$baseUrl/$popular');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> movies = jsonDecode(response.body)["results"];
+
+      for (var movie in movies) {
+        movieInstances.add(MovieModel.fromJson(movie));
+      }
+
+      return movieInstances;
+    }
+
+    throw Error();
+  }
+}
