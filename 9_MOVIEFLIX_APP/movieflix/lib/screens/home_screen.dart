fd3ac2a61433315ba0406a0d5f76008cd0976cc0
rednada1486 <rednada1486@gmail.com>
2024-11-02 22:43:49 +0900

    9-1 feat : home_screen 의 AppBar 부분 코드 작성

--- 9_MOVIEFLIX_APP/movieflix/lib/screens/home_screen.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/screens/home_screen.dart
@@ -5,6 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue, // Colors.white,
+        title: const Text("MovieFlix"),
+      ),
+    );
   }
 }
