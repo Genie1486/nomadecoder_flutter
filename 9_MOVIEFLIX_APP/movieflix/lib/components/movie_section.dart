2c3e1935006d9bbd32f29e2418e394ed1f09c304
rednada1486 <rednada1486@gmail.com>
2024-11-03 01:49:28 +0900

    9-7 인기영화 부분 서식지정

--- 9_MOVIEFLIX_APP/movieflix/lib/components/movie_section.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/components/movie_section.dart
@@ -11,43 +11,58 @@ class MovieSection extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Container(
-        height: 40,
-        alignment: Alignment.centerLeft,
-        child: const Text("Popular Movies"),
-      ),
-      SizedBox(
-        height: 200,
-        child: FutureBuilder(
-          future: movies,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  print(index);
-                  var movie = snapshot.data![index];
-
-                  return Image.network(
-                    movie.posterPath,
-                    width: 350,
-                    height: 217,
-                    fit: BoxFit.cover,
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  width: 40,
-                ),
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          height: 50,
+          alignment: Alignment.topLeft,
+          child: const Text(
+            "Popular Movies",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
-      )
-    ]);
+        SizedBox(
+          height: 200,
+          child: FutureBuilder(
+            future: movies,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    print(index);
+                    var movie = snapshot.data![index];
+
+                    return Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image.network(
+                        movie.posterPath,
+                        width: 330,
+                        height: 230,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const SizedBox(
+                    width: 15,
+                  ),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        )
+      ]),
+    );
   }
 }
