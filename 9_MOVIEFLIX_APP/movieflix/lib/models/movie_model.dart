ef5db7e154ccdb5deb58ae0d4a9b25d1414e2424
rednada1486 <rednada1486@gmail.com>
2024-11-03 01:35:33 +0900

    9-6 fix : posterPath 수정

--- 9_MOVIEFLIX_APP/movieflix/lib/models/movie_model.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/models/movie_model.dart
@@ -2,10 +2,10 @@ class MovieModel {
   final int id;
   final String title, posterPath;
 
-  final String imageServerUrl = "https://image.tmdb.org/t/p/w500";
+  static const String imageServerUrl = "https://image.tmdb.org/t/p/w500";
 
   MovieModel.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         title = json['title'],
-        posterPath = json['poster_path'];
+        posterPath = imageServerUrl + json['poster_path'];
 }
