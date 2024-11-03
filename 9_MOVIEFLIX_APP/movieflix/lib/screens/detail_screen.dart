c2e3945ca7b088cb625df082210706f8bdd116e7
rednada1486 <rednada1486@gmail.com>
2024-11-03 10:39:44 +0900

    9-20 style : 영화 상세 페이지 서식 지정

--- 9_MOVIEFLIX_APP/movieflix/lib/screens/detail_screen.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movieflix/models/movie_detail_model.dart';
 import 'package:movieflix/services/api_service.dart';
+import 'package:movieflix/widgets/star_rating_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final int id;
@@ -27,87 +28,146 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 2, // 밑에 그림자
-          backgroundColor: Colors.transparent, // AppBar의 배경색
-          foregroundColor: Colors.white, // AppBar의 글자색 설정
-          title: const Text(
-            "Back to list",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          leading: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back_ios), // 원하는 아이콘으로 변경
-              onPressed: () {
-                Navigator.pop(context); // 이전 페이지로 돌아가기
-              },
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, // AppBar의 배경색
+        foregroundColor: Colors.white, // AppBar의 글자색 설정
+        title: const Text(
+          "Back to list",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
           ),
         ),
-        body: SingleChildScrollView(
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_ios), // 원하는 아이콘으로 변경
+            onPressed: () {
+              Navigator.pop(context); // 이전 페이지로 돌아가기
+            },
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: true, // 앱바 뒤에 배경 설정
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               const SizedBox(
                 height: 200,
               ),
-              const Column(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Bohemian Rhapsody'),
-                  SizedBox(
+                  const Text(
+                    'Bohemian Rhapsody',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
                     height: 10,
                   ),
-                  Row(
+                  const Row(
                     children: [
-                      Icon(Icons.star),
-                      Icon(Icons.star),
-                      Icon(Icons.star),
-                      Icon(Icons.star),
-                      Icon(Icons.star),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Text("2h 14min"),
-                      Text("|"),
-                      Text("Biography, Drama, Music"),
-                    ],
-                  ),
-                ],
-              ),
-              const Row(
-                children: [
-                  Column(
-                    children: [
-                      Text("StoryLine"),
-                      SizedBox(
-                        height: 20,
+                      StarRating(
+                        rating: 9,
+                        size: 24,
+                        spacing: 7,
                       ),
-                      Text("asdfasfasfdafdafafda"),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "2h 14min",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 3),
+                        child: Text("|",
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.white.withOpacity(0.8),
+                            )),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Biography, Drama, Musicddddddddddddddddddddddddddddddddddddddd",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ],
               ),
               const SizedBox(
-                height: 200,
+                height: 40,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "StoryLine",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 150,
+                    child: Text(
+                      "asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                      overflow: TextOverflow.ellipsis, // 오버플로우 처리
+                      maxLines: 7, // 최대 줄 수 설정
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 35,
               ),
               Container(
                 width: 300,
                 height: 60,
                 decoration: BoxDecoration(
                   color: const Color(0xFFf8d849),
-                  borderRadius: BorderRadiusDirectional.circular(10),
+                  borderRadius: BorderRadiusDirectional.circular(15),
                 ),
-                child: const Center(child: Text("Buy ticket")),
+                child: const Center(
+                    child: Text(
+                  "Buy ticket",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                )),
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
