a6ee28b10e9655e126a9a61ffe84b9d680af2a5a
rednada1486 <rednada1486@gmail.com>
2024-11-03 11:22:14 +0900

    9-22 feat : movie_detail_view에 API로 받은 영화 상세 정보 이력하기

--- 9_MOVIEFLIX_APP/movieflix/lib/screens/detail_screen.dart
+++ 9_MOVIEFLIX_APP/movieflix/lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movieflix/models/movie_detail_model.dart';
 import 'package:movieflix/services/api_service.dart';
+import 'package:movieflix/widgets/movie_detail_view.dart';
 import 'package:movieflix/widgets/star_rating_widget.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -49,124 +50,22 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
       ),
       extendBodyBehindAppBar: true, // 앱바 뒤에 배경 설정
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 200,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Bohemian Rhapsody',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Row(
-                    children: [
-                      StarRating(
-                        rating: 9,
-                        size: 24,
-                        spacing: 7,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "2h 14min",
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: Colors.white.withOpacity(0.8),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 3),
-                        child: Text("|",
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Colors.white.withOpacity(0.8),
-                            )),
-                      ),
-                      Expanded(
-                        child: Text(
-                          "Biography, Drama, Musicddddddddddddddddddddddddddddddddddddddd",
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.white.withOpacity(0.8),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "StoryLine",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    height: 150,
-                    child: Text(
-                      "asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                      overflow: TextOverflow.ellipsis, // 오버플로우 처리
-                      maxLines: 7, // 최대 줄 수 설정
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              Container(
-                width: 300,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFf8d849),
-                  borderRadius: BorderRadiusDirectional.circular(15),
-                ),
-                child: const Center(
-                    child: Text(
-                  "Buy ticket",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                )),
-              ),
-            ],
-          ),
-        ),
+      body: FutureBuilder(
+        future: movie,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MovieDetailView(
+                id: widget.id,
+                runtime: snapshot.data!.runtime,
+                voteAverage: snapshot.data!.voteAverage,
+                title: snapshot.data!.title,
+                overview: snapshot.data!.overview,
+                backDropPath: snapshot.data!.backDropPath,
+                genres: snapshot.data!.genres);
+          }
+
+          return Container();
+        },
       ),
     );
   }
